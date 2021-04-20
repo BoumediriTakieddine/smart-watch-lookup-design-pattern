b3c2103cbe0a5e51997da99f69131075854d4664
BoumediriTakieddine <etaki89@gmail.com>
2021-04-20 02:44:13 +0100

    gui service interface

---
+++ util.service.gui/src/main/java/util/service/gui/GuiService.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util.service.gui;
+
+/**
+ *
+ * @author BOUMEDIRI
+ */
+public interface GuiService {
+    public void showTime();
+    public void activate();
+    public void desactivate();
+    public void zoomIn();
+    public void zoomOut(); 
+}
